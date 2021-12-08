8c43d1af3c05c49a1753ead521b066c7947d33a7
justinng01 <ngjustin0106@gmail.com>
2021-12-08 16:17:03 +0800

    Display found devices

--- app/src/main/java/com/example/locky/Deliver.java
+++ app/src/main/java/com/example/locky/Deliver.java
@@ -7,7 +7,9 @@ import android.content.pm.PackageManager;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.ListFragment;
 
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -17,20 +19,24 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Objects;
 
 /**
  * A simple {@link Fragment} subclass.
  * Use the {@link Deliver#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class Deliver extends Fragment {
+public class Deliver extends ListFragment {
 
     private BluetoothAdapter bluetoothAdapter;
     private final ArrayList<BluetoothDevice> listItems = new ArrayList<>();
     private ArrayAdapter<BluetoothDevice> listAdapter;
+//    protected View mView;
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
@@ -39,6 +45,8 @@ public class Deliver extends Fragment {
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
+
+
 
     public Deliver() {
         // Required empty public constructor
@@ -62,22 +70,64 @@ public class Deliver extends Fragment {
         return fragment;
     }
 
+//    ListView l;
+//    @Override
+//    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+//                             Bundle savedInstanceState) {
+//        // Inflate the layout for this fragment
+//
+////        super.onCreate(savedInstanceState);
+////        setHasOptionsMenu(true);
+//        Log.i("item count", String.valueOf(listItems.stream().count()));
+//        View view = inflater.inflate(R.layout.fragment_deliver, container, false);
+//        Log.i("Create", "Fragment created");
+////        this.mView = view;
+//
+//        listAdapter = new ArrayAdapter<BluetoothDevice>(getActivity(), R.layout.fragment_deliver , listItems) {
+//            @NonNull
+//            @Override
+//            public View getView(int position, View view, @NonNull ViewGroup parent) {
+//                Log.i("view", "getting view");
+//                BluetoothDevice device = listItems.get(position);
+////                if (view == null)
+////                    view = getActivity().getLayoutInflater().inflate(R.layout.fragment_deliver, parent, false);
+//                TextView text1 = view.findViewById(R.id.text1);
+//                TextView text2 = view.findViewById(R.id.text2);
+//                text1.setText(device.getName());
+//                text2.setText(device.getAddress());
+//                return view;
+//            }
+//        };
+//
+//        Log.i("listadapter", String.valueOf(listAdapter.getCount()));
+//
+//
+//        return view;
+//
+//
+//    }
+
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+        setHasOptionsMenu(true);
+//        if (getArguments() != null) {
+//            mParam1 = getArguments().getString(ARG_PARAM1);
+//            mParam2 = getArguments().getString(ARG_PARAM2);
+//        }
         if(getActivity().getPackageManager().hasSystemFeature(PackageManager.FEATURE_BLUETOOTH))
             bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        Log.i("bluetooth", "enabled");
+        Log.i("item count", String.valueOf(listItems.stream().count()));
         listAdapter = new ArrayAdapter<BluetoothDevice>(getActivity(), 0, listItems) {
             @NonNull
             @Override
-            public View getView(int position, View view, @NonNull ViewGroup parent) {
+            public View getView(int position, View view, @NonNull ViewGroup container) {
+                Log.i("view", "getting view");
                 BluetoothDevice device = listItems.get(position);
                 if (view == null)
-                    view = getActivity().getLayoutInflater().inflate(R.layout.fragment_deliver, parent, false);
+                    view = getActivity().getLayoutInflater().inflate(R.layout.fragment_deliver, container, false);
                 TextView text1 = view.findViewById(R.id.text1);
                 TextView text2 = view.findViewById(R.id.text2);
                 text1.setText(device.getName());
@@ -85,18 +135,20 @@ public class Deliver extends Fragment {
                 return view;
             }
         };
+        Log.i("listadapter", String.valueOf(listAdapter.getCount()));
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        Log.i("Create", "Fragment created");
-        super.onCreate(savedInstanceState);
-        setHasOptionsMenu(true);
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        setListAdapter(null);
+        View header = getActivity().getLayoutInflater().inflate(R.layout.fragment_deliver, null, false);
+        getListView().addHeaderView(header, null, false);
+        setEmptyText("initializing...");
+        //Toast.makeText(getActivity(), "initializing...", Toast.LENGTH_SHORT).show();
 
-
-        return inflater.inflate(R.layout.fragment_deliver, container, false);
+//        ((TextView) getListView().getEmptyView()).setTextSize(18);
+        setListAdapter(listAdapter);
     }
 
     @Override
@@ -104,6 +156,63 @@ public class Deliver extends Fragment {
         inflater.inflate(R.menu.menu_devices, menu);
         if (bluetoothAdapter == null)
             menu.findItem(R.id.bt_settings).setEnabled(false);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        if (bluetoothAdapter == null) {
+            setEmptyText("<bluetooth not supported>");
+            Log.i("state", "not supported");
+        }
+        else if (!bluetoothAdapter.isEnabled()) {
+            setEmptyText("<bluetooth is disabled>");
+            Log.i("state", "disabled");
+        }
+        else {
+            setEmptyText("<no bluetooth devices found>");
+            Log.i("state", "running");
+        }
+        Log.i("refresh", "refresh");
+        refresh();
+    }
+
+    public void setEmptyText(@Nullable CharSequence text) {
+    }
+
+
+    void refresh() {
+        listItems.clear();
+        if (bluetoothAdapter != null) {
+            for (BluetoothDevice device : bluetoothAdapter.getBondedDevices()) {
+                if (device.getType() != BluetoothDevice.DEVICE_TYPE_LE) {
+                    if (device.getAddress().equals("08:3A:F2:52:25:A2") | device.getAddress().equals("7C:9E:BD:61:E6:C2")) {
+//                        if(device.getName().substring(0,6).equalsIgnoreCase("LOCKER")) {
+                        Log.i("found", device.getName());
+                        listItems.add(device);
+                        Log.i("items", String.valueOf(listItems.stream().count()));
+//                        }
+                    }
+                }
+            }
+        }
+        Collections.sort(listItems, Deliver::compareTo);
+        listAdapter.notifyDataSetChanged();
+        Log.i("length", String.valueOf(listAdapter.getCount()));
+
+    }
+
+    static int compareTo(BluetoothDevice a, BluetoothDevice b) {
+        boolean aValid = a.getName() != null && !a.getName().isEmpty();
+        boolean bValid = b.getName() != null && !b.getName().isEmpty();
+        if (aValid && bValid) {
+            int ret = a.getName().compareTo(b.getName());
+            if (ret != 0) return ret;
+            return a.getAddress().compareTo(b.getAddress());
+        }
+        if (aValid) return -1;
+        if (bValid) return +1;
+        return a.getAddress().compareTo(b.getAddress());
     }
 
     @Override
